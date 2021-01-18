6d67dd2d3a8c1784606ac9bc47c754243fcdec6a
johninkorea <john829829@gmail.com>
2021-01-18 14:37:40 +0900

    create check_acc.py

--- ch_dc.py
+++ ch_dc.py
@@ -2,98 +2,45 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
-# https://codetorial.net/tensorflow/classifying_the_cats_and_dogs.html
-
-
-!wget --no-check-certificate \
-https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip \
--O /tmp/cats_and_dogs_filtered.zip
-
 
 import os
 import zipfile
+# https://codetorial.net/tensorflow/classifying_the_cats_and_dogs.html
 
+
+# 이미지 받고 동일 위치에 zip파일 압축해제
+# 자료 다운로드 한번 다운하면 이곳은 주석
+!wget --no-check-certificate \
+https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip \
+-O /tmp/cats_and_dogs_filtered.zip
 local_zip = '/tmp/cats_and_dogs_filtered.zip'
-
 zip_ref = zipfile.ZipFile(local_zip, 'r')
-
 zip_ref.extractall('/tmp')
 zip_ref.close()
 
 
-# 기본 경로
-base_dir = '/tmp/cats_and_dogs_filtered'
 
+
+
+# 기본 경로 설정
+base_dir = '/tmp/cats_and_dogs_filtered'
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
+
 
 # 훈련에 사용되는 고양이/개 이미지 경로
 train_cats_dir = os.path.join(train_dir, 'cats')
 train_dogs_dir = os.path.join(train_dir, 'dogs')
-print(train_cats_dir)
-print(train_dogs_dir)
+
 
 # 테스트에 사용되는 고양이/개 이미지 경로
 validation_cats_dir = os.path.join(validation_dir, 'cats')
 validation_dogs_dir = os.path.join(validation_dir, 'dogs')
-print(validation_cats_dir)
-print(validation_dogs_dir)
-
-
-
-train_cat_fnames = os.listdir( train_cats_dir )
-train_dog_fnames = os.listdir( train_dogs_dir )
-
-print(train_cat_fnames[:5])
-print(train_dog_fnames[:5])
-
-
-
-print('Total training cat images :', len(os.listdir(train_cats_dir)))
-print('Total training dog images :', len(os.listdir(train_dogs_dir)))
-
-print('Total validation cat images :', len(os.listdir(validation_cats_dir)))
-print('Total validation dog images :', len(os.listdir(validation_dogs_dir)))
-
-
-
-%matplotlib inline
-
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-
-nrows, ncols = 4, 4
-pic_index = 0
-
-fig = plt.gcf()
-fig.set_size_inches(ncols*3, nrows*3)
-
-pic_index+=8
-
-next_cat_pix = [os.path.join(train_cats_dir, fname)
-                for fname in train_cat_fnames[ pic_index-8:pic_index]]
-
-next_dog_pix = [os.path.join(train_dogs_dir, fname)
-                for fname in train_dog_fnames[ pic_index-8:pic_index]]
-
-for i, img_path in enumerate(next_cat_pix+next_dog_pix):
-  sp = plt.subplot(nrows, ncols, i + 1)
-  sp.axis('Off')
-
-  img = mpimg.imread(img_path)
-  plt.imshow(img)
-
-plt.show()
 
 
 
 
-
-
-
-
-import tensorflow as tf
-
+#신경망 생성
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
@@ -115,9 +62,8 @@ model.summary()
 
 
 
-
+# 컴파일
 from tensorflow.keras.optimizers import RMSprop
-
 model.compile(optimizer=RMSprop(lr=0.001),
             loss='binary_crossentropy',
             metrics = ['accuracy'])
@@ -126,9 +72,8 @@ model.compile(optimizer=RMSprop(lr=0.001),
 
 
 
-
+# 이미지 데이터 전처리
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 
 train_datagen = ImageDataGenerator( rescale = 1.0/255. )
 test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
@@ -149,7 +94,7 @@ validation_generator =  test_datagen.flow_from_directory(validation_dir,
 
 
 
-
+# 학습 과정 저장
 history = model.fit(train_generator,
                     validation_data=validation_generator,
                     steps_per_epoch=100,
@@ -161,6 +106,8 @@ history = model.fit(train_generator,
 
 
 
+
+# 훈련 과정 시각화
 import matplotlib.pyplot as plt
 
 acc = history.history['accuracy']
@@ -183,36 +130,8 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
-
-
-
-
-
-
-
-import numpy as np
-from google.colab import files
-from keras.preprocessing import image
-
-uploaded=files.upload()
-
-for fn in uploaded.keys():
-
-  path='/content/' + fn
-  img=image.load_img(path, target_size=(150, 150))
-
-  x=image.img_to_array(img)
-  x=np.expand_dims(x, axis=0)
-  images = np.vstack([x])
-
-  classes = new_model.predict(images, batch_size=10)
-
-#   print(classes[0])
-
-  if classes[0]>0:
-    print(fn + " is a dog")
-  else:
-    print(fn + " is a cat")
+# 그래프 이미지로 저장
+plt.savefig('savefig_default.png')
 
 
 
@@ -221,25 +140,10 @@ for fn in uploaded.keys():
 
 
 
+
+
+
+
+# 모델 저장
 from keras.models import load_model
 model.save('/content/drive/MyDrive/Colab Notebooks/codetorial/dog_cat_model.h5')
-
-
-
-
-
-
-
-
-
-new_model = tf.keras.models.load_model('/content/drive/MyDrive/Colab Notebooks/codetorial/dog_cat_model.h5')
-# 불러오는 과젖ㅇ
-# 저 괄호안에 드라이브 마운드한 주소 넣으면 되는 부분
-
-
-
-
-
-
-
-
---
+++ check_acc.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+from google.colab import files
+from keras.preprocessing import image
+
+# https://codetorial.net/tensorflow/classifying_the_cats_and_dogs.html
+
+
+
+
+
+
+
+# 불러오rl
+# 저 괄호안에 드라이브 마운드한 주소 넣으면 되는 부분
+new_model = tf.keras.models.load_model('/content/drive/MyDrive/Colab Notebooks/codetorial/dog_cat_model.h5')
+
+
+
+
+
+
+
+
+
+# 테스트 데이터 입력 확인
+
+uploaded=files.upload()
+
+for fn in uploaded.keys():
+
+  path='/content/' + fn
+  img=image.load_img(path, target_size=(150, 150))
+
+  x=image.img_to_array(img)
+  x=np.expand_dims(x, axis=0)
+  images = np.vstack([x])
+
+  classes = new_model.predict(images, batch_size=10)
+
+#   print(classes[0])
+
+  if classes[0]>0:
+    print(fn + " is a dog")
+  else:
+    print(fn + " is a cat")
